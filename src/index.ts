787c31abdd56e8b1b556dafa1a2bdd4c149da0e8
gitusp <usui@lemo.is>
2019-04-18 22:13:44 +0900

    Scaffold the outline of `exhaustMap`
    - Define the interface for `exhaustMap`
    - Stub the implementation and export the curried version of it

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 /** @license MIT License (c) copyright 2018 original author or authors */
 /** @author YOU */
 
-import { Stream } from '@most/types'
+import { Stream } from "@most/types";
+import { curry2 } from "@most/prelude";
 
-// _ :: Stream e -> Stream e
-const _ = <T>(stream: Stream<T>) => stream
+export const exhaustMap: ExhaustMapFn = curry2(_exhaustMap);
 
-export { _ }
+export type ExhaustMapFn = {
+  <A, B>(fn: (value: A) => Stream<B>, stream: Stream<A>): Stream<B>;
+  <A, B>(fn: (value: A) => Stream<B>): (stream: Stream<A>) => Stream<B>;
+};
+
+function _exhaustMap<A, B>(
+  fn: (value: A) => Stream<B>,
+  stream: Stream<A>
+): Stream<B> {
+  // @ts-ignore TODO: Implement
+  return null;
+}
