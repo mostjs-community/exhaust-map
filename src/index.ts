891a9ce9962bfba700c224ce80cdfd4eef9e3176
gitusp <usui@lemo.is>
2019-04-23 00:52:33 +0900

    Scaffold the stream and sink for `exhaustMap`
    This adds:
    - ExhaustMap
    - ExhaustMapSink
    
    And notes the rest of implementations as TODOs.

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,53 @@
 /** @license MIT License (c) copyright 2018 original author or authors */
 /** @author YOU */
 
-import { Stream } from "@most/types";
+import { Disposable, Scheduler, Sink, Stream, Time } from "@most/types";
+import { disposeBoth } from "@most/disposable";
 import { curry2 } from "@most/prelude";
 
-export const exhaustMap: ExhaustMapFn = curry2(_exhaustMap);
 
-export type ExhaustMapFn = {
+export const exhaustMap: CurriedExhaustMap = curry2(_exhaustMap);
+
+interface CurriedExhaustMap {
   <A, B>(fn: (value: A) => Stream<B>, stream: Stream<A>): Stream<B>;
   <A, B>(fn: (value: A) => Stream<B>): (stream: Stream<A>) => Stream<B>;
-};
+}
 
 function _exhaustMap<A, B>(
   fn: (value: A) => Stream<B>,
   stream: Stream<A>
 ): Stream<B> {
-  // @ts-ignore TODO: Implement
-  return null;
+  return new ExhaustMap(fn, stream);
+}
+
+class ExhaustMap<A, B> implements Stream<B> {
+  constructor(private fn: (value: A) => Stream<B>, private source: Stream<A>) {}
+
+  run(sink: Sink<B>, scheduler: Scheduler): Disposable {
+    const exhaustMapSink = new ExhaustMapSink(this.fn, sink, scheduler);
+    return disposeBoth(
+      exhaustMapSink,
+      this.source.run(exhaustMapSink, scheduler)
+    );
+  }
+}
+
+class ExhaustMapSink<A, B> implements Sink<A> {
+  constructor(
+    private fn: (value: A) => Stream<B>,
+    private sink: Sink<B>,
+    private scheduler: Scheduler
+  ) {}
+
+  // TODO: implement
+  event(time: Time, value: A) {}
+
+  // TODO: implement
+  end(time: Time) {}
+
+  // TODO: implement
+  error(time: Time, error: Error) {}
+
+  // TODO: implement
+  dispose() {}
 }
