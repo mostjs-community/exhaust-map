255d5a09d55b2d2f16fba8d322004de5c5215031
gitusp <usui@lemo.is>
2019-04-26 02:04:19 +0900

    Implement methods for `exhaustMap`
    This adds implementations for `exhaustMap`.
    Although it should have all the implementations and should work,
    some fixes might happen after writing test codes.

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,11 @@
 /** @author YOU */
 
 import { Disposable, Scheduler, Sink, Stream, Time } from "@most/types";
-import { disposeBoth } from "@most/disposable";
+import { disposeBoth, tryDispose } from "@most/disposable";
 import { curry2 } from "@most/prelude";
+import { schedulerRelativeTo, currentTime } from "@most/scheduler";
 
-
+// Curry the internal `_exhaustMap` and export it as an overloaded interface.
 export const exhaustMap: CurriedExhaustMap = curry2(_exhaustMap);
 
 interface CurriedExhaustMap {
@@ -13,6 +14,7 @@ interface CurriedExhaustMap {
   <A, B>(fn: (value: A) => Stream<B>): (stream: Stream<A>) => Stream<B>;
 }
 
+// Define an internal and uncurried interface for `exhaustMap`.
 function _exhaustMap<A, B>(
   fn: (value: A) => Stream<B>,
   stream: Stream<A>
@@ -20,6 +22,8 @@ function _exhaustMap<A, B>(
   return new ExhaustMap(fn, stream);
 }
 
+// `ExhaustMap` is a higher-order stream which receives a map function and a source stream.
+// It skips applying the map function when it has an active inner stream.
 class ExhaustMap<A, B> implements Stream<B> {
   constructor(private fn: (value: A) => Stream<B>, private source: Stream<A>) {}
 
@@ -32,22 +36,101 @@ class ExhaustMap<A, B> implements Stream<B> {
   }
 }
 
-class ExhaustMapSink<A, B> implements Sink<A> {
+// `ExhaustMapSink` receives a map function which returns a stream.
+// It applies the map function to a value from its source stream to get an inner stream.
+// It proxies the events from the inner stream to its upper sink.
+// It manages the lifecycle of its inner stream to skip mapping when it has an active stream.
+class ExhaustMapSink<A, B> implements Sink<A>, Disposable {
+  // An ongoing inner stream's disposable
+  current?: Disposable;
+
+  // Whether the stream itself is ended
+  ended: boolean = false;
+
   constructor(
     private fn: (value: A) => Stream<B>,
     private sink: Sink<B>,
     private scheduler: Scheduler
   ) {}
 
-  // TODO: implement
-  event(time: Time, value: A) {}
+  // Map the value from its source and run the stream as an inner stream when it does not have an active stream.
+  event(t: Time, value: A) {
+    if (this.current === undefined) {
+      const segment = new Segment(t, this, this.sink);
+      // Keep this disposable as its current running one.
+      this.current = this.fn(value).run(
+        segment,
+        schedulerRelativeTo(t, this.scheduler)
+      );
+    }
+  }
 
-  // TODO: implement
-  end(time: Time) {}
+  // Mark it as `ended` then check if it has to propagate its upper sink as `ended`.
+  end(t: Time) {
+    this.ended = true;
+    this._checkEnd(t);
+  }
 
-  // TODO: implement
-  error(time: Time, error: Error) {}
+  // Mark it as `ended` then propagate the error to its upper sink.
+  error(t: Time, error: Error) {
+    this.ended = true;
+    this.sink.error(t, error);
+  }
 
-  // TODO: implement
-  dispose() {}
+  // Propagate `dispose` to its current inner stream.
+  dispose() {
+    this._disposeCurrent(currentTime(this.scheduler));
+  }
+
+  // Try to dispose the current inner stream, then unset the current.
+  _disposeCurrent(t: Time) {
+    if (this.current !== undefined) {
+      tryDispose(t, this.current, this.sink);
+      this.current = undefined;
+    }
+  }
+
+  // Dispose the ended inner stream then check if the outer stream should `end` too.
+  _endInner(t: Time) {
+    this._disposeCurrent(t);
+    this._checkEnd(t);
+  }
+
+  // Dispose the dead inner stream then propagate the error to its upper sink.
+  _errorInner(t: Time, error: Error) {
+    this._disposeCurrent(t);
+    this.sink.error(t, error);
+  }
+
+  // When it is marked as `ended` and does not have any active inner stream, emit `end` to the upper sink.
+  _checkEnd(t: Time) {
+    if (this.ended && this.current === undefined) {
+      this.sink.end(t);
+    }
+  }
+}
+
+// `Segment` receives a base time, an outer sink, and a second-level outer sink.
+// It has its local time so that it should add its base time on emitting values to outside.
+class Segment<A, B> implements Sink<B> {
+  constructor(
+    private time: Time,
+    private outer: ExhaustMapSink<A, B>,
+    private sink: Sink<B>
+  ) {}
+
+  // Propagate the value to its second-level outer sink.
+  event(t: Time, value: B) {
+    this.sink.event(t + this.time, value);
+  }
+
+  // Notify its outer sink of its `end`.
+  end(t: Time) {
+    this.outer._endInner(t + this.time);
+  }
+
+  // Notify its outer sink of its `error`.
+  error(t: Time, error: Error) {
+    this.outer._errorInner(t + this.time, error);
+  }
 }
